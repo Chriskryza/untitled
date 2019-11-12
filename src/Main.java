9a24cbf8b5d4e3cbfea87a8b834d038a6e58d30b
Chriskryza <krauze.krzysztof@wp.pl>
2019-11-12 19:54:19 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("hallo World");
+        System.out.println("publikujemy pierszy program!!!");
 
     }
 }
