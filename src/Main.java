6fdc6c5eacf87c0f4d085265df5e0dcdb113bf63
Chriskryza <57682290+Chriskryza@users.noreply.github.com>
2019-11-12 19:55:24 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hallo World");
         System.out.println("publikujemy pierszy program!!!");
+        
+        System.out.println("zostało zedytowane");
 
     }
 }
